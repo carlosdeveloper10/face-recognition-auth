2e39031a1b2fb9645801346b088ae349aadd4ed4
carlos <carlos.montano.19@outlook.com>
2019-02-26 16:44:18 -0500

    Removing credential and/or data sensitive from repo

--- infrastructure/test-env/IT/integration-test.tf
+++ infrastructure/test-env/IT/integration-test.tf
@@ -1,9 +1,14 @@
-provider "heroku" {
-  email   = "carlos.montano.19@outlook.com"
-  api_key = "94a8686d-7c2e-4f98-82e9-4b1b00e10e11"
-}
-
+variable "api_key" {}
+variable "heroku_email" {}
 variable "heroku_app_name" {}
+variable "aws_secret_key" {}
+variable "aws_access_key" {}
+variable "dburl" {}
+
+provider "heroku" {
+  email   = "${var.heroku_email}"
+  api_key = "${var.api_key}"
+}
 
 resource "heroku_app" "integration-test" {
   name   = "${var.heroku_app_name}"
@@ -11,11 +16,11 @@ resource "heroku_app" "integration-test" {
 
   config_vars {
     port = "8080"
-    dburl = "jdbc:postgresql://ec2-174-129-224-157.compute-1.amazonaws.com:5432/dbd47u60jcgd3o"
+    dburl = "${var.dburl}"
     dbusername = "suinkegzkmwilh"
     dbpassword = "bd78e8b0150ba0c00146cff94fca57e5cbf0799149c42322110237aec363cbcb"
-    AWS_ACCESS_KEY = "AKIAJ6FTA2YRTKBCCLXA"
+    AWS_ACCESS_KEY = "${var.aws_access_key}"
     AWS_REGION = "us-east-1"
-    AWS_SECRET_KEY = "6eT60/Aa/nOBqKnYqutnLMYtUH+lsUnjLA2I1Dop"
+    AWS_SECRET_KEY = "${var.aws_secret_key}"
   }
 }
